a302661ef069ecf01e38d850a2688191af21f0b9
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2020-09-27 12:20:34 +0530

    moving to bat file

--- back.py
+++ back.py
@@ -1,5 +1,7 @@
 import json
 import sqlite3 as sql
+import urllib.parse,urllib.error,urllib.request
+from bs4 import BeautifulSoup
 def check_json():
     with open("coll_data.json") as c:
         data=json.loads(c)
@@ -12,14 +14,12 @@ def check_sql():
     #     return s.fetchall()
     conn=sql.connect('coll_sql.db')
     c=conn.cursor()
-    c.execute("SELECT 'coll_Web' FROM 'collist1' ")
+    c.execute("SELECT * FROM 'collist1' ")
     sql_data=c.fetchall()
     conn.commit()
     conn.close()
     return sql_data
     
-
-
 def write_sql(counselling):
     if counselling=='JAC-Delhi':
         website='https://jacdelhi.nic.in/WebInfo/Page/Page?PageId=1&LangId=P'
@@ -30,9 +30,17 @@ def write_sql(counselling):
     conn=sql.connect('coll_sql.db')
     c=conn.cursor()
     c.execute("INSERT INTO collist1 VALUES (:name,:web)",{'name':counselling,'web':website})
+    # c.execute("DELETE FROM collist1")
     conn.commit()
     conn.close()
            
+        
+def scrap():
+    sql_data=check_sql()
+    for s_data in sql_data:
+        if s_data[0]=='JAC-Delhi'or s_data[0]=='JOSSA':
+            pass
+
 
      
     
