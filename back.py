8a830fc93c1d3ea6c0297887ec82363914052ac2
Rishabh Raghwendra <rishabhraghwendra2002@gmail.com>
2020-09-27 12:45:22 +0530

    back.py updated

--- back.py
+++ back.py
@@ -1,7 +1,5 @@
 import json
 import sqlite3 as sql
-import urllib.parse,urllib.error,urllib.request
-from bs4 import BeautifulSoup
 def check_json():
     with open("coll_data.json") as c:
         data=json.loads(c)
@@ -32,14 +30,12 @@ def write_sql(counselling):
     c.execute("INSERT INTO collist1 VALUES (:name,:web)",{'name':counselling,'web':website})
     # c.execute("DELETE FROM collist1")
     conn.commit()
-    conn.close()
-           
+    conn.close()           
         
-def scrap():
-    sql_data=check_sql()
-    for s_data in sql_data:
-        if s_data[0]=='JAC-Delhi'or s_data[0]=='JOSSA':
-            pass
+def matching(site_name,text):
+    json_data=check_json()
+    if text not in json_data[site_name]:
+        pass
 
 
      
